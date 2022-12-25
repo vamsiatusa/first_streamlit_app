85ef9549b08b770e00ff9c829ba39657a7bdab28
vamsi krishna n <121368095+vamsiatusa@users.noreply.github.com>
2022-12-24 21:13:23 -0600

    Update streamlit_app.py
    added Header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
