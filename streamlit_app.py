d886368dcfaf6a67ab1199108a083877cb46dd15
vamsi krishna n <121368095+vamsiatusa@users.noreply.github.com>
2022-12-29 16:45:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ def get_fruityvice_data(this_fruit_choice):
   except URLError as e:
     streamlit.error()
     
-streamlit.write('The user entered ', fruit_choice)
+streamlit.write('The user entered ', this_fruit_choice)
 
 
 
