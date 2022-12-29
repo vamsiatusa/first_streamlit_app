9d7802ce5fd47c254ef2551cd288c15726ed09fb
vamsi krishna n <121368095+vamsiatusa@users.noreply.github.com>
2022-12-29 16:52:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-streamlit.write('The user entered ', fruit_choice)
+#streamlit.write('The user entered ', fruit_choice)
 
 # create the repeatable code block (called a funtion)
 def get_fruityvice_data(this_fruit_choice):
@@ -37,15 +37,15 @@ def get_fruityvice_data(this_fruit_choice):
   # write your own comment - what does this do?
   return fruityvice_normalized
 
-  #New section to display fruityvice api response
-  streamlit.header("Fruityvice Fruit Advice!")
-  try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-      streamlit.error("Please select a fruit to get information.")
-    else:
-      back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_function)
+#New section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
   except URLError as e:
     streamlit.error()
     
