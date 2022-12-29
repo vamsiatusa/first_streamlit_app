784b94a4f95c5f0444422b5d8fc6616b7d55c38f
vamsi krishna n <121368095+vamsiatusa@users.noreply.github.com>
2022-12-29 16:43:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ def get_fruityvice_data(this_fruit_choice):
     else:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
-except URLError as e:
-  streamlit.error()
+  except URLError as e:
+    streamlit.error()
     
 streamlit.write('The user entered ', fruit_choice)
 
